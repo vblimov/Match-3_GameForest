f34edefa999a328ba64d911a5966eab78344c1eb
vblimov <vblimov@gmail.com>
2021-06-25 16:41:26 +1000

    Initial commit

---
+++ Assets/Scripts/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Match_3_GameForest
+{
+    public static class Program
+    {
+        [STAThread]
+        static void Main()
+        {
+            using (var game = new Game1())
+                game.Run();
+        }
+    }
+}
